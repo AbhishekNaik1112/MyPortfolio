78c9553e28a3811c8220246a675ee0ccaefcf5a6
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-12-20 15:22:25 +0530

    First Commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { LuArrowRightToLine } from "react-icons/lu";
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-red-900 to-gray-900"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-white text-5xl sm:text-6xl font-bold text-center">
+            Hi, My name is Abhishek Naik.
+          </h2>
+          <p className="text-white py-4 max-w-l text-center ">
+            I am proficient in HTML, CSS, and JavaScript, and I'm currently
+            expanding my skills by learning React.JS and Tailwind CSS.
+          </p>
+          <div className="flex justify-center items-center">
+            <button className="bg-red-900 text-white items-center flex py-6 px-3 w-fit gap-1 my-2 rounded-md shadow-lg hover:bg-red-700 scale-105 duration-300 ">
+              Portfolio
+              <span>
+                <LuArrowRightToLine />
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-// import { FaBars, FaTimes } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -26,20 +28,41 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-red-600 bg-black fixed">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-gradient-to-b from-red-900 to-black-200 fixed">
       <div>
-        <h1 className="font-signature text-5xl ml-2">Abhishek</h1>
+        <h1 className="font-signature text-5xl ml-2 cursor-pointer">
+          Abhishek
+        </h1>
       </div>
-      <ul className="flex">
+      <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            className="px-4 cursor-pointer capitalize font-medium text-white hover:text-pink-700 scale-105 duration-300"
           >
             {link}
           </li>
         ))}
       </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer  pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black via-red-900 to-gray-900 text-white">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className=" text-2xl px-4 cursor-pointer capitalize py-6 font-medium text-white hover:text-pink-700 scale-105 duration-300"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
