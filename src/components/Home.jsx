096d5e33b8aaeccd6748da09b43829098d8e3f72
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-12-21 00:52:27 +0530

    home page changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,24 @@
 import React from "react";
-import { LuArrowRightToLine } from "react-icons/lu";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 const Home = () => {
+  const el = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Coder", "Web Developer", "Video Editor", "Student"],
+      startDelay: 150,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
+      loop: true,
+      showCursor: true,
+      cursorChar: "_",
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div
       name="home"
@@ -8,21 +26,12 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-white text-5xl sm:text-6xl font-bold text-center">
+          <h2 className="text-white text-5xl sm:text-6xl font-bold text-center animate-bounce">
             Hi, My name is Abhishek Naik.
           </h2>
-          <p className="text-white py-4 max-w-l text-center ">
-            I am proficient in HTML, CSS, and JavaScript, and I'm currently
-            expanding my skills by learning React.JS and Tailwind CSS.
+          <p className="text-white py-4 max-w-l text-center text-3xl ">
+            <span ref={el}></span>
           </p>
-          <div className="flex justify-center items-center">
-            <button className="bg-red-900 text-white items-center flex py-6 px-3 w-fit gap-1 my-2 rounded-md shadow-2xl hover:bg-red-700 scale-105 duration-300">
-              Portfolio
-              <span>
-                <LuArrowRightToLine />
-              </span>
-            </button>
-          </div>
         </div>
       </div>
     </div>
