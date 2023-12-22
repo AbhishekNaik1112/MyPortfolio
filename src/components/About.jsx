9673ee5c4a1827a3535f369ea0245950cf2e0ff6
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-12-23 00:47:25 +0530

    about  changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,38 +2,38 @@ import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black text-white">
+    <div
+      name="about"
+      className="w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black text-white"
+    >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center items-center w-full h-full">
         <div className="pb-8">
-          <p className="text-6xl font-bold inline border-b-4 border-gray-500 ">About</p>
+          <p className="text-6xl font-bold inline border-b-4 border-gray-500 ">
+            About
+          </p>
         </div>
         <p className="text-xl mt-20">
-          Passionate about the dynamic realm of software development, I am
-          currently immersed in a transformative educational journey at Yenepoya
-          University, pursuing a Bachelor of Computer Applications (Hons) with a
-          focused dedication to Product Software Engineering at Kalvium, set for
-          completion in 2027. My expertise in software development and
-          programming has been cultivated through a blend of relentless
-          self-learning and hands-on industry experience.
+          I specialize in a dynamic blend of technical and creative skills,
+          making me a versatile professional in the digital world. My primary
+          languages include C++, HTML5, CSS3/Tailwind CSS and Javascript/React.JS providing a strong
+          foundation for web development and software engineering. On the
+          creative front, I possess proficiency in graphic design and video
+          editing using tools like Photoshop, Premiere Pro, Illustrator, and
+          Filmora Pro. This allows me to transform ideas into visually stunning and
+          engaging content, whether it's for websites, marketing materials, or
+          multimedia presentations.
         </p>
         <br />
         <p className="text-xl">
-          My fervor lies in creating innovative solutions for real-world
-          challenges, fueled by a combination of problem-solving skills and
-          effective communication. The synergy of my academic foundation and
-          practical exposure equips me with the tools to innovate and address
-          the evolving needs of our interconnected world.
-        </p>
-        <br />
-        <p className="text-xl">
-          Driven by an insatiable thirst for growth, I embrace each opportunity
-          as a chance to expand my skill set and contribute meaningfully to
-          technological and societal progress. Committed to continuous learning
-          and improvement, my ultimate goal is to evolve into a dynamic and
-          adaptable software engineer, making impactful strides in the
-          ever-evolving technology landscape. Join me on this exciting journey
-          as I endeavor to shape the future through my unwavering passion for
-          software development.
+          Complementing my technical and creative abilities are my soft skills,
+          including effective communication, problem-solving, and teamwork. I
+          excel at collaborating with cross-functional teams to deliver
+          innovative solutions and user-friendly designs. In essence, I am a
+          well-rounded professional with a knack for marrying technology and
+          creativity. Whether it's coding for functionality or designing for
+          aesthetics, I bring a comprehensive skill set to the table, ensuring
+          that projects I undertake are both functional and visually
+          captivating.
         </p>
       </div>
     </div>
--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -2,15 +2,24 @@ import React from "react";
 
 const MyWork = () => {
   return (
-    <div
-      name="mywork"
-      className="h-screen w-full bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black"
-    >
-      <div>
-        <div></div>
+    <div name="mywork" className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black w-full text-white md:h-screen">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className=" pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">My Work</p>
+          <p>Some of my work that I made</p>
+        </div>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <button>Demo</button>
+            <button>Code</button>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default MyWork;
+
+
