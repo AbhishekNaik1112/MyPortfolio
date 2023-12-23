366edc3c6a693697bceb56eb850599b9eaf164eb
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-12-24 00:03:45 +0530

    navbar changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,43 +28,49 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-900 via--900 to-black fixed ">
-      <div>
-        <h1 className="font-signature text-5xl ml-2 cursor-pointer">
-          Abhishek
-        </h1>
-      </div>
-      <ul className="hidden md:flex">
-        {links.map(({ id, link }) => (
-          <li
-            key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-white hover:text-red-600 scale-105 duration-300"
-          >
-            {link}
-          </li>
-        ))}
-      </ul>
-      <div
-        onClick={() => setNav(!nav)}
-        className="cursor-pointer  pr-4 z-10 text-gray-500 md:hidden"
-      >
-        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-      </div>
+    <div className="relative">
+      <div className="blur-md absolute inset-1 h-20 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-400 via-gray-600 to-gray-900 background-animate"></div>
 
-      {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black text-white background-animate">
+      <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black fixed background-animate">
+      
+        <div>
+          <h1 className="font-signature text-5xl ml-2 cursor-pointer">
+            Abhishek
+          </h1>
+        </div>
+        
+        <ul className="hidden md:flex">
           {links.map(({ id, link }) => (
             <li
               key={id}
-              className=" text-2xl px-4 cursor-pointer capitalize py-6 font-medium text-white hover:text-red-600 scale-105 duration-300"
+              className="px-4 cursor-pointer capitalize font-medium text-white hover:text-red-600 scale-105 duration-300"
             >
               {link}
             </li>
           ))}
         </ul>
-      )}
+        <div
+          onClick={() => setNav(!nav)}
+          className="cursor-pointer  pr-4 z-10 text-gray-500 md:hidden"
+        >
+          {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </div>
+
+        {nav && (
+          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black text-white background-animate">
+            {links.map(({ id, link }) => (
+              <li
+                key={id}
+                className=" text-2xl px-4 cursor-pointer capitalize py-6 font-medium text-white hover:text-red-600 scale-105 duration-300"
+              >
+                {link}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
-};
+}
 
 export default NavBar;
