4ccc7394c680a64b2e7b4d23251b1386ccc7ab31
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2024-01-09 08:47:32 +0530

    loader added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import MyStack from "./components/MyStack";
-import MyWork from "./components/MyWork";
+import React, { useState, useEffect } from "react";
+import { DNA } from "react-loader-spinner";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import MyWork from "./components/MyWork";
+import MyStack from "./components/MyStack";
+import Contact from "./components/Contact";
 import Socials from "./components/Socials";
 import VerCon from "./components/VerCon";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
-      <MyWork />
-      <MyStack />
-      <Contact />
-      <Socials />
-      <VerCon/>
+      {loading ? (
+        <div className="flex items-center justify-center h-screen bg-black">
+          <DNA
+            visible={true}
+            height="150"
+            width="150"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+        </div>
+      ) : (
+        <>
+          <NavBar />
+          <Home />
+          <About />
+          <MyWork />
+          <MyStack />
+          <Contact />
+          <Socials />
+          <VerCon />
+        </>
+      )}
     </div>
   );
 }
